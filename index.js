fe7029b39c26b2127fd5715ef8f36399c5d241ca
joseleh@gmail.com <joseleh@gmail.com>
2018-08-10 19:14:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {egg: 3,};
+function updateObjectWithKeyAndValue(object,key,value){
+ var newRecipes = Object.assign({},object,{[key]: value});
+ console.log(newRecipes);
+ 
+ return newRecipes;
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object, key) {
+ var nObject = Object.assign({},object);
+ console.log(nObject);
+delete nObject['key'];
+return nObject;
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete Object[key];
+  return object;
+}
