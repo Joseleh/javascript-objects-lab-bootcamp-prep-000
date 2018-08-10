a39ce2df184f7ecde26591980a0bb78a2d0185f6
joseleh@gmail.com <joseleh@gmail.com>
2018-08-10 19:55:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ delete nObject['key'];
 return Object;
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete Object.key;
+  delete Object['key'];
   console.log(Object);
   return Object;
 }
